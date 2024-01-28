8398704df01a9fcc2b2dffda96b67dda2a1f294d
Taeyong Lee <d104601@hotmail.com>
2024-01-28 16:58:00 -0600

    Initial commit using basic material and scaffold

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return buildMaterialApp(context);
+  }
+
+  MaterialApp buildMaterialApp(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: buildScaffold(context),
+    );
+  }
+
+  Scaffold buildScaffold(BuildContext context) {
+    String title = 'I am poor';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.grey
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('assets/image/begging.jpg'),
+        ),
+      ),
+    );
+  }
+}
+
+
